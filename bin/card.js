d2e88caf5cd06346fe942c76cacbcb61f080b792
Heitor Dobeis <heitordobeis@gmail.com>
2019-02-16 18:49:43 -0200

    feat: add email

--- bin/card.js
+++ bin/card.js
@@ -17,12 +17,14 @@ const data = {
     twitter: chalk.cyan('https://twitter.com/heitordobeis'),
     github: chalk.cyan('https://github.com/dobeis'),
     linkedin: chalk.cyan('https://www.linkedin.com/in/dobeis/'),
+    email: chalk.cyan('heitordobeis@gmail.com'),
     web: chalk.cyan('https://dobeis.com/'),
     npx: chalk.white('npx dobeis'),
     labelWork: chalk.white.bold('      Work:'),
     labelTwitter: chalk.white.bold('   Twitter:'),
     labelGitHub: chalk.white.bold('    GitHub:'),
     labelLinkedIn: chalk.white.bold('  LinkedIn:'),
+    labelEmail: chalk.white.bold('     Email:'),
     labelWeb: chalk.white.bold('       Web:'),
     labelCard: chalk.white.bold('      Card:')
 }
@@ -33,9 +35,10 @@ const working = `${data.labelWork}  ${data.work}`
 const twittering = `${data.labelTwitter}  ${data.twitter}`
 const githubing = `${data.labelGitHub}  ${data.github}`
 const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
+const emailing = `${data.labelEmail}  ${data.email}`
 const webing = `${data.labelWeb}  ${data.web}`
 const carding = `${data.labelCard}  ${data.npx}`
 
-const output = heading + newline + newline + working + newline + twittering + newline + githubing + newline + linkedining + newline + webing + newline + newline + carding
+const output = heading + newline + newline + working + newline + twittering + newline + githubing + newline + linkedining + newline + emailing + newline + webing + newline + newline + carding
 
 console.log(chalk.green(boxen(output, options)))
